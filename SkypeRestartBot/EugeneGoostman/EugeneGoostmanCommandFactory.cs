1ea2072a95977ce4a88cb87a502b3cf0b379a40e
mikhail.brinchuk <mikhail.brinchuk@localhost>
2013-04-25 01:17:26 +0400

    Eugene Goostman

---
+++ SkypeRestartBot/EugeneGoostman/EugeneGoostmanCommandFactory.cs
@@ -0,0 +1,26 @@
+﻿using SKYPE4COMLib;
+
+namespace SkypeRestartBot.EugeneGoostman
+{
+    public sealed class EugeneGoostmanCommandFactory : ICommandFactory
+    {
+        public string Url { get; set; }
+
+        public bool CanHandle(string message, out string alias)
+        {
+            alias = message;
+
+            return true;
+        }
+
+        public ICommand CreateCommand(ChatMessage message, Config config, ISkypeMessenger skype)
+        {
+            return new EugeneGoostmanCommand(message, Url, skype);
+        }
+
+        public bool CanHandleNullTarget
+        {
+            get { return true; }
+        }
+    }
+}
